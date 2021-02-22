ea987e8a14683e792c0ab9156afbea5053d31d1c
Benjamin Stanfield <bstanfield1992@gmail.com>
2021-02-21 21:14:10 -0700

    first commit -added variables

---
+++ index.js
@@ -0,0 +1,9 @@
+// Variables
+const inquirer = require("inquirer");
+const fs = require("fs");
+const style = require("./templates/css");
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Manager = require("./lib/manager");
+const Intern = require("./lib/intern");
+
